4b2dd0bf92d76943953811f214de931320588c7f
Vignesh <vigneshrsb99@gmail.com>
2022-07-04 18:09:27 +0800

    Navbar Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { FiberPin } from '@mui/icons-material';
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import {Box} from '@mui/material';
+
+import ExerciseDetail from './pages/ExerciseDetail.js';
+import Home from "./pages/Home.js";
+import NavBar from "./components/NavBar.js";
+import Footer from './components/Footer.js';
 
 function App() {
   return (
-   <div>App</div>
+   <Box width="400px" sx={{width: {xl:'1488px'}}} m="auto">
+    <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />}/>
+      </Routes>
+      <Footer/>
+    </Box>
   );
 }
 
