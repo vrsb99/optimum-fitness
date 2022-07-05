e02d734b079b5af53b3b34aeb10685130119af07
Vignesh <vigneshrsb99@gmail.com>
2022-07-05 09:37:41 +0800

    bug fixes

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import {Box, Button, Stack, TextField, Typography} from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack algignItems="center" mt="37px"
+    justifyContent="center" p="20px">
+
+    </Stack>
   )
 }
 
