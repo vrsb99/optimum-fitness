d6855552c2cb07817010e05dba3ec3980467e42b
Vignesh <vigneshrsb99@gmail.com>
2022-07-10 12:31:14 +0800

    Setup with RAPID API

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,11 +1,48 @@
 import React, {useEffect, useState} from 'react';
 import {Box, Button, Stack, TextField, Typography} from '@mui/material';
 
-const SearchExercises = () => {
-  return (
-    <Stack algignItems="center" mt="37px"
-    justifyContent="center" p="20px">
+import { exerciseOptions,fetchData } from '../utils/fetchData';
 
+const SearchExercises = () => {
+  const [search,setSearch] = useState('');
+  const handleSearch = async () => {
+    if(search) {
+      const exerciseData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      console.log(exerciseData);
+      console.log(exerciseOptions);
+    }
+  }
+
+  return (
+    <Stack alignItems="center" mt="37px"
+    justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{fontSize: {lg: '44px',xs:'30px'}}} mb="50px" textAlign="center">
+        Awesome Exercises you <br />
+        Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+      <TextField
+      sx={
+        {
+          input: {fontWeight: '700', border: 'none', borderRadius: '4px'},
+          width: {lg: '800px', xs: '350px'},
+          backgroundColor: "#fff",
+          borderRadius: "40px"
+        }
+      }
+      height="76px"
+      value={search}
+      onChange={(input)=> {setSearch(input.target.value.toLowerCase())}}
+      placeholder="Search Exercises"
+      type="text"
+      />
+      <Button className="search-btn" sx={{bgcolor: '#FF2625', color: "#fff", textTransform: "none", width: {lg: '175px', xs: '80px'},
+          fontsize: {lg: '20px', xs: '14px'}, height: '56px', position: "absolute", right:"0"}}
+          onClick={handleSearch}
+          >
+        Search
+      </Button>
+      </Box>
     </Stack>
   )
 }
